63a5eb92925670e8a3b7c55c5a3e1a28a9686b20
COZULEMEZ <info.cozulemez@gmail.com>
2026-04-12 00:11:57 +0300

    fix: resolve 404 errors for sub-directories and multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3001',
-                changeOrigin: true,
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        legal: resolve(__dirname, 'legal.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        thanks: resolve(__dirname, 'thanks.html'),
+        tr_index: resolve(__dirname, 'tr/index.html'),
+        tr_legal: resolve(__dirname, 'tr/legal.html'),
+        tr_privacy: resolve(__dirname, 'tr/privacy.html'),
+        tr_thanks: resolve(__dirname, 'tr/thanks.html'),
+      },
     },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
 });
