98ad7b0fb07e87b80bf81fd53bc3b9f3ded91e85
COZULEMEZ <info.cozulemez@gmail.com>
2026-07-03 15:10:53 +0300

    UI: Remove video from home page and improve mobile navbar aesthetics

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSettings } from '../../context/SettingsContext';
 import Hero from '../../sections/Hero';
 import Stats from '../../sections/Stats';
-import Video from '../../sections/Video';
 import Features from '../../sections/Features';
 import './index.css';
 
@@ -11,7 +10,6 @@ const Home = () => {
 
   return (
     <div className="page" style={{ width: '100%' }}>
-      <Video />
       <Hero settings={settings} />
       <Stats />
       <Features settings={settings} />
